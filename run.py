9cb7799e369e9ffd9f7f02f3264d4c5597099775
Rick <rickatherton316@gmail.com>
2022-03-02 10:37:50 +0000

    Fix style issues and bugs within code so far

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-import gspread # Importing to open and edit pizza ordering spreadsheet
+import gspread  # Importing to open and edit pizza ordering spreadsheet
 from google.oauth2.service_account import Credentials
-from tabulate import tabulate # Importing tabulate to present data to the user clearly
-import re # Importing re module for validating mobile phone number
+from tabulate import tabulate  # Importing to present data to the user clearly
+import re  # Importing re module for validating mobile phone number
 
 
 SCOPE = [
@@ -15,6 +15,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('vv_pizzas')
 
+
 class Customer:
     """
     Customer data class
@@ -24,29 +25,31 @@ class Customer:
         Request and validate customers name and telephone number
         """
         while True:
-            name = input(f"Please provide your name:\n").lower()
-            if name.isalpha(): # Validates the customer as entered a character based name
+            name = input("Please provide your name:\n").lower()
+            if name.isalpha():  # Validates the customer is characters only
                 break
             else:
-                print(f"Invalid name, please try again\n")  
-            return name  
+                print("Invalid name, please try again\n")
+            return name
         print(f"Hi {name.capitalize()}\n")
-            
-        """
-        Request and validate customers mobile phone number 
-        """
+
         def validate_mobile(telnum):
-            numPattern = re.compile("(0)?[0-9]{11}") # Validates number to begin with 0 and be 11 digits only
-            return numPattern.match(telnum)
-            
+            """
+            Request and validate customers mobile phone number.
+            Number must begin with 0 and be 11 digits.
+            """
+            num_pattern = re.compile("(0)?[0-9]{11}")
+            return num_pattern.match(telnum)
+
         while True:
-            telnum = input(f"Please provide a mobile contact number:\n")
-            if (validate_mobile(telnum)):
+            telnum = (input("Please provide a mobile contact number:\n"))
+            if validate_mobile(telnum):
                 break
             else:
-                print(f"Invalid number. Exactly 11 digits required, starting with 0, please try again\n")
-            return telnum    
+                print("Invalid number. 11 digits required, starting with 0, please try again\n")
+            return telnum
         print(f"Thanks {name.capitalize()}, we will use {telnum} to contact you if there's any issue with your order.\n")
+
 
 class Topping:
     """
@@ -54,7 +57,8 @@ class Topping:
     """
     def get_topping():
         """
-        Present the customer with the choice of toppings and request a choice of 1-4
+        Present the customer with the choice of toppings,
+        and request a choice of 1-4
         """
         print("Here are todays choices of toppings, which would you like?")
         print("[1] Margherita")
@@ -62,56 +66,56 @@ class Topping:
         print("[3] Diavolo")
         print("[4] Forza\n")
 
-        """
-        Request and validate the customers choice is between 1-4
-        """
+        # Request and validate the customers choice is between 1-4
         while True:
             try:
-                topping = int(input(f"Please choose a topping by typing the corresponding number and clicking enter:\n"))
+                topping = int(input("Please choose a topping by typing the corresponding number and clicking enter:\n"))
             except ValueError:
-                print(f"Invalid choice, please enter a number\n")  
+                print("Invalid choice, please enter a number\n")
                 continue
-            if topping in range(1, 5, 1): # change to a data dictionary so the return print message is a string value
+            # change to a data dict to return print msg is a string value
+            if topping in range(1, 5, 1):  
                 print(f"Thanks for choosing a {topping}\n")
             else:
-                print(f"Invalid choice, please try again\n") 
+                print("Invalid choice, please try again\n")
                 continue
             return topping
-         
+
+
 class Size:
     """
     Pizza topping size
     """
     def get_size():
         """
-        Present the customer with the choice of sizes and request a choice S, M, L
+        Present the customer with the choice of sizes,
+        and request a choice S, M, L
         """
         print("Which size of pizza would you like?")
         print('[S] Small 7"')
         print('[M] Medium 10"')
         print('[L] Large 14"')
 
-        """
-        Request and validate the customers choice is either S, M or L
-        """
+        #  Request and validate the customers choice is either S, M or L
         while True:
-            size = input(f"Please choose a size by entering the corresponding letter and clicking enter:\n")
+            size = input("Please choose a size by entering the corresponding letter and clicking enter:\n")
             if size == ('S').lower():
-                print(f"Thanks for choosing Small\n")
+                print("Thanks for choosing Small\n")
             elif size == ('M').lower():
-                print(f"Thanks for choosing Medium\n")
+                print("Thanks for choosing Medium\n")
             elif size == ('L').lower():
-                print(f"Thanks for choosing Large\n")
+                print("Thanks for choosing Large\n")
             else:
-                print(f"Invalid choice, please enter either S, M or L\n")
+                print("Invalid choice, please enter either S, M or L\n")
                 continue
             return size
+
 
 class Order:
     """
     Customer order class
     """
-    def confirm_order():
+    def confirm_order(self, cust_order):
         """
         Confirm the order back to the customer
         ask the customer to proceed Y/N
@@ -119,53 +123,51 @@ class Order:
         """
         while True:
             # need to code how to get the values from the other classes - do we pass them into the main() function?
-            currentOrder = input(f"{Customer.name} you have ordered a {Size.size} {Topping.topping} is this ready to be sent to the kitchen? Y/N:\n")
-            if currentOrder == ('Y').lower():
-                print(f"Thanks. Sending your order to Vera in the kitchen...\n")
+            cust_order = input(f"{Customer.name} you have ordered a {Size.size} {Topping.topping} is this ready to be sent to the kitchen? Y/N:\n")
+            if cust_order == ('Y').lower():
+                print("Thanks. Sending your order to Vera in the kitchen...\n")
                 orders_worksheet = SHEET.worksheet("Orders")
-                orders_worksheet.append_row(custOrder)
-                print(f"Your order has been received, please come and collect in 20 minutes.\n See you soon!\n")
-            elif currentOrder == ('N').lower():
-                moreItems = input(f"That's ok. Would you like to order more items? Y/N\n") 
-                if moreItems == ('Y').lower():
+                orders_worksheet.append_row(cust_order)
+                print("Your order has been received, please come and collect in 20 minutes.\n See you soon!\n")
+            elif order == ('N').lower():
+                more_items = input("That's ok. Would you like to order more items? Y/N\n")
+                if more_items == ('Y').lower():
                     print("I need to code how to add more items")
-                elif moreItems == ('N').lower():
-                    amendOrder = input(f"Would you like to amend this order? Y/N\n")
-                    if amendOrder == ('Y').lower():
+                elif more_items == ('N').lower():
+                    amend_order = input("Would you like to amend this order? Y/N\n")
+                    if amend_order == ('Y').lower():
                         print("I need to code how to amend the order")
-                    elif amendOrder == ('N').lower():
+                    elif amend_order == ('N').lower():
                         confirm_order()
                     else:
-                        print(f"Invalid choice, please enter either Y or N\n")
+                        print("Invalid choice, please enter either Y or N\n")
                         continue
                 else:
-                    print(f"Invalid choice, please enter either Y or N\n")
-                    continue      
-            else: 
-                print(f"Invalid choice, please enter either Y or N\n")
+                    print("Invalid choice, please enter either Y or N\n")
+                    continue
+            else:
+                print("Invalid choice, please enter either Y or N\n")
                 continue
-            
-            
-        return f"Thanks Customer.name(self) for ordering a Size.size(self) Topping.topping(self)"
+            return f'{"Thanks Customer.name(self) for ordering a Size.size(self) Topping.topping(self)"}'
 
 
 
-#def update_order_worksheet(custOrder):
-    #print(f"Thanks. Sending your order to Vera in the kitchen...\n")
-    #orders_worksheet = SHEET.worksheet("Orders")
-    #orders_worksheet.append_row(custOrder)
-    #print(f"Your order has been received, please come and collect in 20 minutes.\n See you soon!\n")
+# def update_order_worksheet(custOrder):
+    # print(f"Thanks. Sending your order to Vera in the kitchen...\n")
+    # orders_worksheet = SHEET.worksheet("Orders")
+    # orders_worksheet.append_row(custOrder)
+    # print(f"Your order has been received, please come and collect in 20 minutes.\n See you soon!\n")
 
 
 def main():
     """
     Run all program functions
     """
-    custData = Customer.get_customer_data()
-    custTopping = Topping.get_topping()
-    custSize = Size.get_size()
-    custOrder = Order.confirm_order()
-    #update_order_worksheet(custOrder)
+    cust_data = Customer.get_customer_data()
+    cust_topping = Topping.get_topping()
+    cust_size = Size.get_size()
+    cust_order = Order.confirm_order()
+    # update_order_worksheet(custOrder)
 
 
 print("Thank you for choosing Vera's Vegan Pizzas!\n")
